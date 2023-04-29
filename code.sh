f16f892591e901edff8fd263782c4d9c6a84f4f4
Henriksin <heneka2009@gmail.com>
2023-04-29 16:06:48 +0000

    modified code.sh

--- code.sh
+++ code.sh
@@ -1,7 +1,7 @@
-This is my second repo
-i will try to finish my coding class
+This is my fifth changes
 i will come out and add more
-remote job loading soon
 i am becoming good
 i will make small recommendations
-
+sjjsskks
+skllddmds
+ks j  dndd
